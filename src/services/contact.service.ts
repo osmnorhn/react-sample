6eccbe8b56f0e191085470ff7ccdf893751aa7dd
Osman Orhan <oso@dhcp-168-wla-so.intomics.com>
2020-05-25 16:36:18 +0200

    Contact service and interface type for contact changes are committed

---
+++ src/services/contact.service.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../types/contact";
+import { IRequestService } from "./request.service";
+
+export class ContactService {
+  constructor(private request: IRequestService) {}
+  public async getContacts() {
+    let data = await new Promise<ContactResponse>((resolve, reject) => {
+      fetch(`http://localhost:3000/contact`)
+        .then((response) => resolve(response.json()))
+        .catch((reason) => {
+          reject(reason);
+          // log the error here
+          console.log(reason);
+        });
+    });
+
+    return data as ContactResponse;
+  }
+}
+
+/////////////////////////////////
+// API RESPONSE DATA PROTOTYPE //
+/////////////////////////////////
+
+interface ContactResponse {}
---
+++ src/types/contact.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+    firstName: string;
+      lastName: string;
+      email: string;
+      company: string;
+      phone: string;
+      created_date: Date
+}
